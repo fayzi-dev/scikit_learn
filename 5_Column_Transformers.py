011d9f205095d67f4c9d1ec9f37aa000e9610832
fayzi-dev <fayzi.dev@gmail.com>
2024-10-17 19:36:17 +0330

    Passthrough & Drop in Columntransformer

--- 5_Column_Transformers.py
+++ 5_Column_Transformers.py
@@ -1,14 +1,15 @@
+import numpy as np
 import pandas as pd
-
-df = pd.read_csv('/home/m-fayzi/Desktop/scikit_learn/Datasets/titanic.csv')
-# print(df.info())
-X = df[['Fare','Embarked','Sex','Age']]
-# print(X.head())
-
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.compose import make_column_transformer
 from sklearn.compose import make_column_selector
+
+# df = pd.read_csv('/home/m-fayzi/Desktop/scikit_learn/Datasets/titanic.csv')
+# print(df.info())
+# X = df[['Fare','Embarked','Sex','Age']]
+# print(X.head())
+
 
 
 ohe = OneHotEncoder()
@@ -49,8 +50,46 @@ imp = SimpleImputer()
 
 # ct = make_column_transformer((imp, ['Age']))
 
-ct = make_column_transformer((ohe, make_column_selector(pattern='S')))
+# ct = make_column_transformer((ohe, make_column_selector(pattern='S')))
 
 # ct = make_column_transformer((ohe, make_column_selector(dtype_include=object)))
 # ct = make_column_transformer((ohe, make_column_selector(dtype_include='number')))
-print(ct.fit_transform(X))
+# print(ct.fit_transform(X))
+
+data = pd.DataFrame({
+    'A':[1, 2, np.nan],
+    'B':[11,12,13],
+    'C':[14,15,16],
+    'D':[17,18,19]
+})
+
+print(data)
+# output :
+#      A    B     C      D
+# 0  1.0  100  1000  10000
+# 1  2.0  200  2000  20000
+# 2  NaN  300  3000  30000
+
+# ct = make_column_transformer(
+#     (imp, ['A']),
+#     ('passthrough', ['B','C']),
+#     remainder='drop'
+# )
+
+# print(ct.fit_transform(data))
+# output
+# [[ 1.  11.  14. ]
+#  [ 2.  12.  15. ]
+#  [ 1.5 13.  16. ]]
+
+ct = make_column_transformer(
+    (imp, ['A']),
+    ('drop', ['D','C']),
+    remainder='passthrough'
+)
+
+print(ct.fit_transform(data))
+# output:
+# [[ 1.  11. ]
+#  [ 2.  12. ]
+#  [ 1.5 13. ]]
